bd1210521b79bf5c85b9b073f3a010353a0b2408
felijoao08 <felipesouza09099@gmail.com>
2025-03-26 13:12:04 -0300

    melhorando header

--- Portifolio Felipe/header.js
+++ Portifolio Felipe/header.js
@@ -2,7 +2,5 @@
 document.getElementById('hamburguer').addEventListener('click', function() {
     // Alterna a visibilidade do menu de navegação e contatos
     const navbar = document.querySelector('.navbar');
-    const contatosNavbar = document.querySelector('.contatos_navbar');
     navbar.classList.toggle('active');
-    contatosNavbar.classList.toggle('active');
 });
