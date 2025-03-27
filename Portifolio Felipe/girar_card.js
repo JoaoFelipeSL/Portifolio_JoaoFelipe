bccd26796f638283671e563edcc2d904aa58bf77
felijoao08 <felipesouza09099@gmail.com>
2025-03-27 16:52:51 -0300

    Projetos

--- Portifolio Felipe/girar_card.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll(".flip-btn").forEach(button => {
-    button.addEventListener("click", () => {
-        let card = button.closest(".card");
-        card.classList.toggle("flipped");
-        button.textContent = card.classList.contains("flipped") ? "Voltar" : "Saber mais";
-    });
-});
