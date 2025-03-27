c16f098e50eb940f336d1a7e6653976a0303227e
felijoao08 <felipesouza09099@gmail.com>
2025-03-27 20:35:34 -0300

    carrossel de projetos

---
+++ Portifolio Felipe/carrosselProjetos.js
@@ -0,0 +1,24 @@
+let currentIndex = 0;
+
+function moveCarousel(direction) {
+    const track = document.querySelector(".carousel-track");
+    const items = document.querySelectorAll(".carousel-item");
+    const totalItems = items.length;
+    const visibleItems = 1; // Número de itens visíveis por vez
+
+    currentIndex += direction;
+    if (currentIndex < 0) {
+        currentIndex = totalItems - visibleItems;
+    } else if (currentIndex > totalItems - visibleItems) {
+        currentIndex = 0;
+    }
+
+    const offset = -(currentIndex * (200 + 20)); // 200px de largura + 20px de margem
+    track.style.transform = `translateX(${offset}px)`;
+}
+
+function toggleImage(item) {
+    const images = item.querySelectorAll("img");
+    images[0].classList.toggle("hidden");
+    images[1].classList.toggle("hidden");
+}
